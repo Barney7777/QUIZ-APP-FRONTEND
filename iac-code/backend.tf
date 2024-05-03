9606559aea0d5e4bfbf92f05d053c34673f8168f
Barney Wang <wangyaxu7@gmail.com>
2024-05-03 20:44:42 +1000

    add frontend codes

--- iac-code/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    # don't use variables here, just use values
-    bucket         = "barney-terraform-remote-state"
-    key            = "frontend/dev/terraform.tfstate"
-    region         = "ap-southeast-2"
-    dynamodb_table = "barney-terraform-state-lock"
-  }
-}
